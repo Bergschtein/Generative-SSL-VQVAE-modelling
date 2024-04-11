622bd488a6aa7c164339a2dfb8777a321941850c
Bergschtein <42778982+Bergschtein@users.noreply.github.com>
2024-04-11 15:53:10 +0200

    Corrections to exp

--- experiments_augs2.py
+++ experiments_augs2.py
@@ -12,13 +12,13 @@ from trainers.train_maskgit import train_maskgit
 import torch
 
 # Wandb logging information
-STAGE1_PROJECT_NAME = "Stage1-Augmentations"
+STAGE1_PROJECT_NAME = "Stage1-Augmentations-WinWarp-AmpResize"
 # STAGE2_PROJECT_NAME = "Final-Stage2-Gaussian"
 # STAGE2_MINI_PROJECT_NAME = "Final-Stage2-Mini-Gaussian"
 # Stage 1 experiments to run
 STAGE1_EXPS = [
-    "byol"
-]  # , "vibcreg", "barlowtwins"]  # empty string means regular VQVAE
+    "","byol","vibcreg", "barlowtwins"
+]   # , "vibcreg", "barlowtwins"]  # empty string means regular VQVAE
 # Datasets to run experiments on
 UCR_SUBSET = [
     # "ElectricDevices",
@@ -37,7 +37,7 @@ NUM_RUNS_PER = 1
 # Controls
 RUN_STAGE1 = True
 RUN_STAGE2 = False
-RUN_MINI_STAGE2 = True
+RUN_MINI_STAGE2 = False
 SEED = 2
 # Epochs:
 STAGE1_EPOCHS = 1000
